2ef071c2a7e14d5cdb9fb4e9e0b8e837ce27476d
nicolasteri <nicolasteri93@gmail.com>
2021-06-14 10:51:03 +0200

    importo ed utilizzo Pickle per salvare i dati in un file: creo funzioni salva e leggi, e le eseguo

--- azienda_manifatturiera/main.py
+++ azienda_manifatturiera/main.py
@@ -1,3 +1,5 @@
+import pickle 
+
 class Modello():
     def __init__(self, nome_modello, descrizione):
         self.nome_modello = nome_modello
@@ -14,6 +16,9 @@ class Dipartimenti():
     def __init__(self, nome_dipartimento):
         self.nome_dipartimento = nome_dipartimento
         self.modelli = []
+        #aggiungo Pickle
+        self.file = self.nome_dipartimento + ".p"
+
 
     def aggiungi(self, modello):
         self.modelli.append(modello)
@@ -38,6 +43,19 @@ class Dipartimenti():
             else:
                 print("Modello non trovato !")
 
+
+###############  creo funzioni Pickle   ################
+
+    def salva(self):     ### aggiunge i valori al file sopra creato
+        with open(self.file, "wb") as file:
+            pickle.dump(self.modelli, file)
+        print("Salvataggio effettuato")
+
+    def leggi(self):     ### legge i valori del file sopra creato
+        with open(self.file, "rb") as file:
+            self.modelli = pickle.load(file)
+        
+########################################################
     
 
 modello1 = Modello("plastica", "modello utile per creare piccole strutture solide")    
@@ -57,3 +75,13 @@ dipartimento1.aggiungi(modello1)
 dipartimento1.stampa_dipartimenti()
 print("------------------------\n")
 dipartimento1.cerca("alluminio")
+
+dipartimento1.salva()
+
+### eseguo solo funzione Leggi per leggere il file sopra creato e salvato con valori
+
+'''
+print("\n---------- LETTURA FILE ----------")
+dipartimento1.leggi()
+dipartimento1.stampa_dipartimenti()
+'''
