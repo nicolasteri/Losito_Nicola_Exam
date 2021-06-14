2195c57da79088dcae5d2f815a9861b99ed500bb
nicolasteri <nicolasteri93@gmail.com>
2021-06-14 09:59:14 +0200

    creo classe Dipartimenti con funzioni aggiungi, elimina e stampa lista

--- azienda_manifatturiera/main.py
+++ azienda_manifatturiera/main.py
@@ -1,6 +1,6 @@
 class Modello():
-    def __init__(self, nome, descrizione):
-        self.nome = nome
+    def __init__(self, nome_modello, descrizione):
+        self.nome_modello = nome_modello
         self.descrizione = descrizione
 
     def modifica_descrizione(self, nuova_desc):
@@ -10,4 +10,26 @@ class Modello():
     def stampa_modello(self):
         print("Nome: %s\nDescrizione: %s\n\n" %(self.nome, self.descrizione))
 
+class Dipartimenti():
+    def __init__(self, nome_dipartimento):
+        self.nome_dipartimento = nome_dipartimento
+        self.modelli = []
 
+    def aggiungi(self, modello):
+        self.modelli.append(modello)
+        print("\nModello aggiunto correttamente !")
+
+    def elimina(self, modello):
+        if modello in self.modelli:
+            self.modelli.remove(modello)
+            print("\nModello rimosso con successo !")
+        else:
+            print("\nModello non presente !")
+
+    def stampa_dipartimenti(self):
+        for modello in self.modelli:
+            modello.stampa_modello()
+
+    
+
+    
